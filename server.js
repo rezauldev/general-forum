f4e1adfe5437c743ddd84e990f539bb3f3b6421e
rezaul choudhury <rofy2009@gmail.com>
2022-07-01 23:17:28 +0100

    linked pages are to another page!

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')
 })
