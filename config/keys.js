fbc57b902d549ee6a1fd4cd2806cf1db956e88e7
rezaul choudhury <rofy2009@gmail.com>
2022-08-22 01:07:32 +0100

    questions.ejs file been modified!

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://general-forum:DG5lvhfBfzzoph51@cluster0.juzjcnb.mongodb.net/general-forum?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://general-forum:9AgZWvhXfQQ545EY@cluster0.fqmiha5.mongodb.net/general-forum?retryWrites=true&w=majority'
 }
